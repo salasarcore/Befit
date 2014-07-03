635632540a21704a7e282d11821b10c347ef7981
Swati Dhake <swatid@salasar.co>
2014-07-03 18:28:25 +0530

    no message

--- admin/conn.php
+++ admin/conn.php
@@ -1,16 +1,14 @@
 <?php
 
 ini_set('date.timezone', 'Asia/Calcutta');
-$hostname= $_SERVER["HTTP_HOST"];
 $hostname=str_replace("www.","",$hostname);
 $urlParts = explode('.', $hostname);
 $subdomainname= $urlParts[0];
-$link=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("befit_ladies_gym");
+$link=mysql_connect ("localhost", "root", "sou_mendas") or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ("befit");
 //define("js", "http://alpha.se.dev.salasarserver.com");
 define("SITE_URL_BRANCH_ADMIN", 'http://localhost/befit1/admin');
 @session_start();
 $rowsPerPage=20;
 $currency="Rs";
-Define("DOMAIN_IDENTIFIER",$subdomainname);
 ?>
